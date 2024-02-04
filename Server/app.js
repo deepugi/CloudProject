d0f9b35c2990c6958a92463d02cc9d86d7a31702
deepugi <munideepav@gmail.com>
2024-02-04 12:45:07 -0500

    modified

--- Server/app.js
+++ Server/app.js
@@ -72,6 +72,13 @@ app.get('/userDetails', (req, res) => {
     }
 });
 
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
